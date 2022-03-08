b6ce92dd456d2fbe58bcb18d87cc181fa9734f19
Mondesir SAHI <mondesir.sahi@HARRELL.LAN>
2022-03-08 13:22:09 +0000

    init sahi

---
+++ database/migrations/2022_02_03_232540_create_devis_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('num', 45);
+            $table->string('slug', 250)->nullable();
+            $table->integer('duree_validite');
+            $table->datetime('date_fin');
+
+            $table->integer('id_recorder')->unsigned();
+            $table->integer('id_client')->unsigned();
+            $table->integer('id_entreprise')->unsigned();
+        
+            $table->foreign('id_recorder')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('id_client')->references('id')->on('clients')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            
+            $table->foreign('id_entreprise')->references('id')->on('entreprises')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devis');
+    }
+}
+
